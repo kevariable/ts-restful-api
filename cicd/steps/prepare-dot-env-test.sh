82a29153d7bde824b2f6aa654807a7abbf187ad3
Kevin Abrar Khansa <kevariable@gmail.com>
2024-04-17 15:46:34 +0700

    fix env

--- cicd/steps/prepare-dot-env-test.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-ENV_FILE=".env"
-
-cp .env.example .env
--- cicd/steps/run-containers.sh
+++ cicd/steps/run-containers.sh
@@ -5,6 +5,8 @@ DC="docker-compose -f docker-compose.ci.yml"
 
 ls -l
 
+cp .env.example .env
+
 ${DC} up -d app mysql
 
 export COMPOSE_INTERACTIVE_NO_CLI=1
